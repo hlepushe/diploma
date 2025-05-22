aed585c3bf01e90364bd792cbd4170bb7712595e
Liza <146002062+hlepushe@users.noreply.github.com>
2025-05-22 12:05:31 +0300

    Add files via upload

---
+++ qq_plot_manh_plot base.R
@@ -0,0 +1,46 @@
+library(data.table)
+library(qqman)
+
+gemma_results <- fread("gwas_bush_60.assoc.txt", header = TRUE)
+
+
+gemma_results$chr <- as.character(gemma_results$chr)
+
+chrom_map <- setNames(as.character(1:14), paste0("lcl|Bpe_Chr", 1:14))
+
+gemma_results$chr <- chrom_map[gemma_results$chr]
+gemma_results$chr <- as.numeric(gemma_results$chr)
+
+
+
+
+manhattan(gemma_results, chr = "chr", bp = "ps", snp = "rs", p = "p_wald", 
+          col = c("#00aeef", "#00395d"), 
+          main = "full data kerel-none", 
+          ylim = c(0, 10), cex.axis = 1.5)
+
+qq(gemma_results$p_wald, cex.axis = 2, cex.lab = 1)
+
+
+#двигает трешхолд:
+
+n_tests_10 <- nrow(gemma_results) 
+bonf_threshold_10 <- -log10(0.05/n_tests_10)
+
+
+manhattan(gemma_results,            chr = "chr",
+                                    bp = "ps",
+                                    snp = "rs",            
+                                    p = "p_wald",                         
+                                    col = c("#00aeef", "#00395d"),
+                                    main = "подпись",
+                                    genomewideline = F,
+                                    #bonferroni_line = F,
+                                    ylim = c(0,8),
+                                    cex.axis = 1.5,
+                                    abline(h = bonf_threshold_10, col = "red"))
+ 
+
+
+
+
